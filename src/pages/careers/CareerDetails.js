86d365f0d6791872fb86c9af0d8b59e0a70414f9
jgelsana <jgelsana@77global.biz>
2024-09-15 16:15:20 +0800

    lesson8 progress

---
+++ src/pages/careers/CareerDetails.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useLoaderData, useParams } from 'react-router-dom'
+
+export default function CareerDetails() {
+
+    const { id } = useParams()
+
+    const career = useLoaderData()
+
+return (
+    <div className="career-details">
+    <h2>Career Details for {career.title}</h2>
+    <p>Starting Salary: {career.salary}</p>
+    <p>Location: {career.location}</p>
+    <div className="details">
+        <p>Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Fuga voluptas veritatis
+        reiciendis? Reprehenderit accusamus tempore
+        reiciendis amet nemo eum saepe sapiente eius
+        nesciunt aut! Velit maxime dolorum qui mollitia?
+        Natus.</p>
+    </div>
+    </div>
+
+  )
+}
+
+// loader function
+export const careerDetailsLoader = async ({ params }) => {
+    const { id } = params
+
+    const res = await fetch('http://localhost:4000/careers' + id)
+
+    return res.json();
+}
